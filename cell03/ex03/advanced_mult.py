bab10a72027458ee49991e4612cc94d73e7ff654
kthawee <65050093@kmitl.ac.th>
2025-09-16 12:08:28 +0700

    Update advanced_mult.py

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -9,5 +9,5 @@ if len(sys.argv) > 1:
 for i in range(11):
     print(f"Table de {i}:", end=" ")
     for j in range(11):
-        print(i * j, end=" " if j < 10 else "")
+        print(i * j, end=" ")
     print()
